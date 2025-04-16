7aa18b5d42a508d7407f43b98ea913c23af3d8e8
raghav <raghapb34n@gmail.com>
2025-04-16 22:14:47 +0530

    updated the api to use API KEY and ping

--- pages/api/scrape.ts
+++ pages/api/scrape.ts
@@ -3,21 +3,38 @@ import { NextApiRequest, NextApiResponse } from "next"
 import puppeteer from "puppeteer-core"
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const browser = await puppeteer.launch({
-        args: chrome.args,
-        defaultViewport: chrome.defaultViewport,
-        executablePath: await chrome.executablePath(),
-        headless: "new" as any,
-    })
+    if (req.method === "POST") {
 
-    const page = await browser.newPage();
-    await page.goto("https://www.plunge.one", {
-        waitUntil: "networkidle2"
-    });
+        if (req.query.ping === "true") {
+            res.status(200).json({message: "pong!"});
+            return;
+        }
 
-    const content = await page.content();
+        const k = req.body["api_key"];
 
-    await browser.close();
+        if (k !== process.env.API_KEY) {
+            res.status(403).json({message: "Unauthorized"});
+            return;
+        }
+        const browser = await puppeteer.launch({
+            args: chrome.args,
+            defaultViewport: chrome.defaultViewport,
+            executablePath: await chrome.executablePath(),
+            headless: "new" as any,
+        })
 
-    res.status(200).json({html: content});
+        const page = await browser.newPage();
+        await page.goto("https://www.plunge.one", {
+            waitUntil: "networkidle2"
+        });
+
+        const content = await page.content();
+
+        await browser.close();
+
+        res.status(200).json({html: content});
+
+    } else {
+        res.status(405).json({message: "method not allowed"})
+    }
 }
